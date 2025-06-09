00d5a2fb40ecf768ac15f4433979d1f362c01d34
ZAYStudio <zaystudio03@gmail.com>
2025-06-09 15:05:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+<script async="async" data-cfasync="false" src="//pl26873016.profitableratecpm.com/e972ef0593e138e043c08dcb606f1f65/invoke.js"></script>
+<div id="container-e972ef0593e138e043c08dcb606f1f65"></div>
